67ccf3166f4372d3bef7e4ffaca14dae6eeb078e
arturoalbero <aag400@gmail.com>
2026-06-28 11:26:39 +0200

    edit index

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 define('LARAVEL_START', microtime(true));
 
 // Detectar entorno local o producción según existencia de carpeta o archivo
